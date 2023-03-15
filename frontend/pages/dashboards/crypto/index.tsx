40762fc44efc4fd3144fad19b005c7ea18f4ca30
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2023-03-15 10:26:16 -0300

    refactor: remove unncessary

--- frontend/pages/dashboards/crypto/index.tsx
+++ frontend/pages/dashboards/crypto/index.tsx
@@ -5,12 +5,8 @@ import SidebarLayout from '@/layouts/SidebarLayout';
 import PageHeader from '@/content/Dashboards/Crypto/PageHeader';
 import PageTitleWrapper from '@/components/PageTitleWrapper';
 import { Container, Grid } from '@mui/material';
-import Footer from '@/components/Footer';
 
-import AccountBalance from '@/content/Dashboards/Crypto/AccountBalance';
 import Wallets from '@/content/Dashboards/Crypto/Wallets';
-import AccountSecurity from '@/content/Dashboards/Crypto/AccountSecurity';
-import WatchList from '@/content/Dashboards/Crypto/WatchList';
 
 function DashboardCrypto() {
   return (
@@ -29,21 +25,11 @@ function DashboardCrypto() {
           alignItems="stretch"
           spacing={4}
         >
-          <Grid item xs={12}>
-            <AccountBalance />
-          </Grid>
           <Grid item lg={8} xs={12}>
             <Wallets />
           </Grid>
-          <Grid item lg={4} xs={12}>
-            <AccountSecurity />
-          </Grid>
-          <Grid item xs={12}>
-            <WatchList />
-          </Grid>
         </Grid>
       </Container>
-      <Footer />
     </>
   );
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -68,14 +68,7 @@ function Overview() {
       <Hero />
       <Container maxWidth="lg" sx={{ mt: 8 }}>
         <Typography textAlign="center" variant="subtitle1">
-          Crafted by{' '}
-          <Link
-            href="https://bloomui.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            BloomUI.com
-          </Link>
+          Crafted by LiV
         </Typography>
       </Container>
     </OverviewWrapper>
