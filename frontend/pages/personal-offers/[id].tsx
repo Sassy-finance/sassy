1e61c488371dc7133681c1d4120f6852499469a9
Ialberquilla <ivan.alberquilla@gmail.com>
2023-03-22 11:08:26 +0100

    show qr

--- frontend/pages/personal-offers/[id].tsx
+++ frontend/pages/personal-offers/[id].tsx
@@ -1,9 +1,25 @@
 import RecentOffers from '@/content/personal-offers/detail/RecentOffers';
 import SidebarLayout from '@/layouts/SidebarLayout';
+import { useRouter } from 'next/router'
+import { getPendingClaims } from '../../src/api/backend'
 
 import { Grid, Container } from '@mui/material';
 
 function PersonalOfferDetail() {
+
+  const router = useRouter()
+  const { id } = router.query
+
+  console.log(id)
+
+
+  const getData = async () => {
+    const claims = await getPendingClaims()
+    const claim = claims.find(claim => claim.id === id)
+    console.log(claim)
+  }
+
+
   return (
     <>
       <Container maxWidth="lg" style={{ margin: '2rem 0' }}>
