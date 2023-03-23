d5812a6c4c130f9014f7a21b4daf2c641bf996e1
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2023-03-23 18:16:24 -0300

    refactor: cards

--- frontend/src/content/personal-nfts/CardContent/index.tsx
+++ frontend/src/content/personal-nfts/CardContent/index.tsx
@@ -1,42 +1,57 @@
-import { Box, CardContent, Typography } from "@mui/material";
-import { useState } from "react";
-import ShowQRModal from "./ShowQRModal";
+import { Box, CardContent, Typography, styled } from '@mui/material';
+import { useState } from 'react';
+import ShowQRModal from './ShowQRModal';
+
+const CardTitle = styled(Typography)(
+  () => `
+    margin-top: 1rem;
+    color: #da18a2;
+    text-align: center;
+  `
+);
 
 interface NFTsCardContentProps {
   name: string;
-  amount: string;
   value: string;
+  amount: string;
 }
 
-function NFTsCardContent({ name, amount, value }: NFTsCardContentProps) {
-  const [openModal, setOpenModal] = useState(false)
+function NFTsCardContent({ name, value, amount }: NFTsCardContentProps) {
+  const [openModal, setOpenModal] = useState(false);
 
   const handleOpenModal = (): void => {
-    console.log("handle")
+    console.log('handle');
     setOpenModal(true);
-  }
+  };
 
   return (
     <>
-      <CardContent onClick={()=> handleOpenModal()}>
-        <Typography variant="h3" noWrap marginTop={'1rem'}>
+      <CardContent onClick={() => handleOpenModal()}>
+        <CardTitle variant="h3" noWrap sx={{ mb: 2 }}>
           {name}
-        </Typography>
-        <Box sx={{ pt: 3 }}>
-          <Typography variant="subtitle2">
-            {`Max Liabilities: ${amount}`}
-          </Typography>
-          <Typography variant="subtitle2">
-            {`Min Collateral: ${value}`}
-          </Typography>
+        </CardTitle>
+        <Box sx={{ pt: 3, px: 1 }}>
+          <Box sx={{ mb: 2 }}>
+            <Typography variant="h4" noWrap>
+              {`${value}`}
+            </Typography>
+            <Typography variant="subtitle2">Max Debt</Typography>
+          </Box>
+          <Box sx={{ mb: 2 }}>
+            <Typography variant="h4" noWrap>
+              {`${amount}`}
+            </Typography>
+            <Typography variant="subtitle2">Minimum Balance</Typography>
+          </Box>
         </Box>
       </CardContent>
       <ShowQRModal
-        handleClose={() => { }}
-        handleInputChange={() => { }}
-        handleSubmit={() => { }}
+        handleClose={() => {}}
+        handleInputChange={() => {}}
+        handleSubmit={() => {}}
         open={openModal}
-        tier={name} />
+        tier={name}
+      />
     </>
   );
 }
